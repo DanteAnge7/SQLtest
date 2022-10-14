ce7521e0e56f1d604e97ad156733f523f6ef784a
DanteAnge7 <114161171+DanteAnge7@users.noreply.github.com>
2022-10-14 15:39:55 +0200

    Add files via upload

---
+++ request.sql
@@ -0,0 +1,15 @@
+--Selection des films
+SELECT * FROM film;
+--Modif description du film
+UPDATE film SET 35 = 'The Matrix' WHERE 35 = '';
+
+--inseré nouveau film
+
+INSERT TO categorie (`nom`) VALUES (`Science-Fiction`, `Fantastic`, `Horreur`),
+SELECT 43 FROM commentaire WHERE `id_film`, `id_user`;
+
+--supprimé le film
+
+DELETE FROM
+
+
